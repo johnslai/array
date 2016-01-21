aaad8c3ba4852f6be3ea22bbeb85e0c026b21813
John Lai <john.lai@dopplerlabs.com>
2016-01-21 14:20:49 -0800

    fixed compile error, and tested

--- array.h
+++ array.h
@@ -1,6 +1,8 @@
 #ifndef _ARRAY_H_
 #define _ARRAY_H_
+#include <stdint.h>
 
-extern unsigned char array[];
+#define ARRAY_SIZE 32
+extern uint8_t array[ARRAY_SIZE];
 
 #endif
--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <string.h>
+#include <stdint.h>
+#include <stdio.h>
 #include "array.h"
 
+#define _TEST_
 int main()
 {
-  char memory[1000];
-  char *p = memory;
+  uint8_t memory[1000];
+  uint8_t *p = memory;
   // copy all data from array to destination
   memcpy(p, array, sizeof(array));
+#ifdef _TEST_
+  printf("sizeof(array) = %lu\n", sizeof(array));
+  for (int i = 0; i< sizeof(array); i++) {
+    if (p[i] != array[i]) 
+      printf("error @ %d expected %d = %d\n", i, p[i], array[i]);
+  }
+#endif
   return 0;
 }
